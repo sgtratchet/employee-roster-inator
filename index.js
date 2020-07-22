bccc6cf657033e69f73b81044aa53bd314984258
Devin ellsworth <ellsworths@Sabrinas-Mac-mini.local>
2020-07-21 21:18:47 -0600

    started questions

---
+++ index.js
@@ -0,0 +1,43 @@
+const inquirer = require('inquirer');
+const createEngineer = require('./lib/Engineer');
+const createIntern = require('./lib/Intern');
+const createManager = require('./lib/Manager')
+
+const promptUser = ()=>{
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: 'What is the team Managers name?',
+            validate: managerNameInput => {
+                if (managerNameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter a name!');
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'What is the team Managers Email?'
+        },
+        {
+            type: 'input',
+            name: 'managerPhone',
+            message: 'What is the team Managers office phone number?'
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'What is the team Managers Employee ID?'
+        },
+        {
+            type: 'list',
+            name: 'employeeAdd',
+            message: 'Please select an option, add team member or finish',
+            choices: [Engineer, Intern, finish]
+        }
+    ]);
+};
